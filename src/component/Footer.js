5d168ce27d4b82ce0e8cb1f24100eebb213dbc8d
bh20511 <bh20511@gmail.com>
2023-05-10 17:06:46 +0800

    footer外觀修改

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,13 +1,52 @@
 import React from "react";
+import styled from "../styles/footer.module.scss"
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer
-      className="text-center text-white"
-      style={{ backgroundColor: "#f1f1f1" }}
-    >
-      <div className="container pt-4">
-        <section className="mb-4">
+    <>
+    <footer className={styled.mainFooter}>
+      <div className={styled.container}>
+        <div className={styled.footerItem}>
+          <h4>關於我們</h4>
+          <nav>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>關於我們</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>門市據點</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>聯絡我們</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>加入我們</Link>
+          </nav>
+        </div>
+
+        <div className={styled.footerItem}>
+          <h4>合作品牌</h4>
+          <nav>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>寵物公園</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>毛孩市集</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>凱朵寵物</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>貓狗隊長</Link>
+          </nav>
+        </div>
+
+        <div className={styled.footerItem}>
+          <h4>服務項目</h4>
+          <nav>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>寵物零食</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>寵物美容</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>寵物診所</Link>
+            <Link href="/"><i class="fa-solid fa-chevron-right"></i>寵物住宿</Link>
+          </nav>
+        </div>
+
+        <div className={`${styled.footerItem} ${styled.footerSubs}`}>
+          <h4>訂閱電子報</h4>
+          <form >
+            <input type="text" />
+            <input type="submit" value="訂閱電子報"/>
+          </form>
+        </div>
+      </div>
+      <div >
+      <section className={styled.row}>
           <a
             className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
@@ -63,17 +102,10 @@ const Footer = () => {
           </a>
         </section>
       </div>
-
-      <div
-        className="text-center text-dark p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-      >
-        © 2023 Copyright:
-        <a className="text-dark" href="https://mdbootstrap.com/">
-          github/bh20511
-        </a>
-      </div>
+      <div className={styled.copyright}>&copy; 2023 Copyright github/bh20511</div>
     </footer>
+    </>
+    
   );
 };
 
